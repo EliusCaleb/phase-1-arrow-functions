05074197c041862d96b73cf44e5172f46b9fac62
ELIUS <eliuswanyoro@gmail.com>
2022-09-06 16:38:35 +0300

    Done

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-divide = (doMaths) => 2000/100;
+//let divide = (doMaths) => 2000/100;
+let divide =()=> 2000/100 ; 
 
 let square = (x) => x * x; let x = 2;
 
- add =(a,b) => a + b;  let a = 3; let b = 4
+
+let  add =(a,b) => a + b;  let a = 3; let b = 4
+
+
+
+
